7f65fab8333a4ad2d53af3710dcff37b94ad5607
bpbrainiak <ulises.ruz@gmail.com>
2013-04-16 21:44:13 -0300

    Se agrega proyecto utilitario

---
+++ ActiveRecordGenerator/ActiveRecordGenerator.Utilities/RegisteredConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveRecordGenerator.Utilities
+{
+    public class RegisteredConnectionString
+    {
+        public int Id { get; set; }
+        public string ConnectionStringName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
